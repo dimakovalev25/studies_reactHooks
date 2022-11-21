8a9d2781ddcd191990f12414886dbdd483c25f3a
dimakovalev25 <d.nozdryn@gmail.com>
2022-11-21 15:29:25 +0300

    useCallback

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useCallback} from "react";
 import {Container} from "react-bootstrap";
 
 const calc = () => {
@@ -21,17 +21,18 @@ const Slider = (props) => {
     }
 
     useEffect(() => {
-        console.log('useEffect')
+        // console.log('useEffect')
         document.title = `Slide: ${slide}`
     }, [slide])
 
-    const getSomeImg = () => {
+    const getImg = useCallback(() => {
         console.log('get img')
         return [
             "https://slon.fr/wp-content/uploads/2020/07/393721-svetik-1280x960.jpg",
             "https://slon.fr/wp-content/uploads/2020/07/393721-svetik-1280x960.jpg"
         ]
-    }
+    }, [])
+
 
     return (
         <Container>
@@ -40,19 +41,20 @@ const Slider = (props) => {
                      src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
                      alt="slide"/>
 
-                {
-                    getSomeImg().map((url, i) => {
-                        return (
-                            <img
-                                key={i}
-                                className="d-block w-100"
-                                src={url}
-                                alt="slide"/>
-                        )
-                    })
+                {/*{*/}
+                {/*    getImg().map((url, i) => {*/}
+                {/*        return (*/}
+                {/*            <img*/}
+                {/*                key={i}*/}
+                {/*                className="d-block w-100"*/}
+                {/*                src={url}*/}
+                {/*                alt="slide"/>*/}
+                {/*        )*/}
+                {/*    })*/}
 
-                }
+                {/*}*/}
 
+                <Slide getImg={getImg} />
 
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}
                 </div>
@@ -77,6 +79,31 @@ const Slider = (props) => {
             </div>
         </Container>
     )
+}
+
+const Slide = ({getImg}) => {
+    const [img, setImg] = useState([]);
+
+    useEffect(() => {
+        setImg(getImg())
+    }, [getImg])
+
+    return (
+        <>
+            {img.map((url, i) => {
+                return (
+                    <img
+                        key={i}
+                        className="d-block w-100"
+                        src={url}
+                        alt="slide"/>
+                )
+            })
+
+            }
+        </>
+    )
+
 }
 
 export default Slider;
